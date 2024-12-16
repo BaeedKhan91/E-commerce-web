fa27584e03433a5a2efff21031798f57ab9dd2f7
Baeed khan <baeedk7@gmail.com>
2024-12-16 13:46:33 +0500

    complete home page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { assets } from '../assets/frontend_assets/assets'
+
+function Footer() {
+  return (
+    <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-4 my-10 mt-40 text-sm'>
+        <div>
+            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <p className='w-full md:w-2/3 text-gray-600 '>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo culpa ullam, dolores voluptates fugit nihil! Laborum harum ducimus minima deleniti cumque aliquam, quo omnis amet, aspernatur, deserunt dolores ut voluptates.
+            </p>
+        </div>
+        <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Delivery</li>
+                <li>Privacy Policy</li>
+            </ul>
+        </div>
+        <div>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>+92-316-2364385</li>
+                <li>Baeedk7@gmail.com</li>
+            </ul>
+        </div>
+      </div>
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'> CopyRight 2024@ forever.com all Right Reserved</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Latestcollection from '../components/Latestcollection'
+import BestSeller from '../components/BestSeller'
+import OurPolicy from '../components/OurPolicy'
+import NewsLetterBox from '../components/NewsLetterBox'
 
 function home() {
   return (
     <div>
+      
       <Hero/>
+      <Latestcollection/>
+      <BestSeller/>
+      <OurPolicy/>
+      <NewsLetterBox/>
     </div>
   )
 }
