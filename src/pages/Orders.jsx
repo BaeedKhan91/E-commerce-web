e3e39244932f5314db1e73099e5e5b3236b2d9af
Muhammad Bilal <bilaljawed245@gmail.com>
2024-12-21 16:41:07 +0500

    making order page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Titlle from './../components/Title'
 
 function Orders() {
+
+  const {products ,currency} =useContext (ShopContext)
+
   return (
-    <div>
-      
+    <div className='border-t pt-16 '>
+      <div className='text-2xl'>
+        <Titlle text1={'MY'} text2={"ORDERS"}/>
+      </div>
+
+      <div>
+        {
+          products.slice(1,4).map((item,index)=>(
+            <div key={index} className='py-4 border-t text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4'>
+              <div className='flex items-start text-sm gap-6'>
+                <img className='' src={item.image[0]} alt="" />
+              </div>
+            </div>
+          ))
+        }
+      </div>
+
     </div>
   )
 }
